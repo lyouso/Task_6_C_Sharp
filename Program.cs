e4131271bee61d41e409a66fe2a2e7e26cb0810c
Liubov K <lyouso@yandex.ru>
2022-03-10 12:58:05 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 0) Console.WriteLine("Вы ввели 0");
+else {
+    if (number % 2 == 0) Console.WriteLine("Да, число является четным");
+    else Console.WriteLine("Нет, число является нечетным");
+}
